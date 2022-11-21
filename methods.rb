10c09493ed383ef200746aef3ff994b956cf33d4
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-11-21 13:44:45 -0500

    commit tree 60c8ed28fa9a9b63b11ca0bf0283a8f638b7c488

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
-# Your code here!
+# Yourode here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet ("Naureen")
+
+def greet_with_default(name="Naureen")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+
+
+
